50e2856728c6a2a5b922acda004436f92fbe989e
maartendegoede <maartendegoede@appstudio.nl>
2017-10-17 17:44:47 +0200

    Forgot to commit one file..

--- app/src/main/java/eu/insertcode/portfolio/ProjectsActivity.kt
+++ app/src/main/java/eu/insertcode/portfolio/ProjectsActivity.kt
@@ -2,9 +2,14 @@ package eu.insertcode.portfolio
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ImageView
-import com.bumptech.glide.Glide
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import eu.insertcode.portfolio.data.CategoryItem
 import eu.insertcode.portfolio.data.Item
+import eu.insertcode.portfolio.data.ProjectItem
+import eu.insertcode.portfolio.data.SubcategoryItem
+import eu.insertcode.portfolio.widgets.Category
+import eu.insertcode.portfolio.widgets.Project
 
 
 class ProjectsActivity : AppCompatActivity() {
@@ -16,9 +21,26 @@ class ProjectsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_projects)
 
-        val imageView = findViewById<ImageView>(R.id.project_image)
-        Glide.with(this).asBitmap().load("https://portfolio.insertcode.eu/media/game_unity_mgfm1.png").into(imageView)
+        val layout: LinearLayout = findViewById(R.id.projects_root)
+
+        val items = items ?: return
+        loadProjects(layout, items)
     }
 
-
+    private fun loadProjects(parent: ViewGroup, items: List<Item>) {
+        for (item in items.iterator()) {
+            if (item is CategoryItem) {
+                val category = Category(item, this)
+                parent.addView(category)
+                loadProjects(category.findViewById(R.id.category_content), item.items)
+            }
+            if (item is SubcategoryItem) {
+                //TODO: add sub to view
+                loadProjects(parent, item.items)
+            }
+            if (item is ProjectItem) {
+                parent.addView(Project(item, this))
+            }
+        }
+    }
 }
