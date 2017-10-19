bd7d9bc9c78edc999b8bcf224312c98e62f5dbb1
maartendegoede <maartendegoede@appstudio.nl>
2017-10-19 14:07:53 +0200

    Added rounded image sides.

---
+++ app/src/main/java/eu/insertcode/portfolio/utils/BitmapUtil.kt
@@ -0,0 +1,44 @@
+package eu.insertcode.portfolio.utils
+
+import android.content.Context
+import android.graphics.*
+
+/**
+ * Created by maartendegoede on 19/10/17.
+ * Copyright © 2017 AppStudio.nl. All rights reserved.
+ */
+class BitmapUtil {
+    companion object {
+
+        fun getRoundedCornerBitmap(ctx: Context, input: Bitmap, pixels: Int, width: Int, height: Int) = getRoundedCornerBitmap(ctx, input, pixels, width, height, false, false, false, false)
+        fun getRoundedCornerBitmap(ctx: Context, input: Bitmap, pixels: Int, width: Int, height: Int, TL: Boolean, TR: Boolean, BL: Boolean, BR: Boolean): Bitmap {
+            val output = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(output)
+            val densityMultiplier = ctx.resources.displayMetrics.density
+
+            val color = 0xff424242
+            val paint = Paint()
+            val rect = Rect(0, 0, width, height)
+            val rectF = RectF(rect)
+
+            val roundPx = pixels * densityMultiplier
+
+            paint.isAntiAlias = true
+            canvas.drawARGB(0, 0, 0, 0)
+            paint.color = color.toInt()
+            canvas.drawRoundRect(rectF, roundPx, roundPx, paint)
+
+            val h = height.toFloat()
+            val w = width.toFloat()
+            if (TL) canvas.drawRect(0f, h / 2, w / 2, h, paint)
+            if (TR) canvas.drawRect(w / 2, 0f, w, h / 2, paint)
+            if (BL) canvas.drawRect(0f, 0f, w / 2, h / 2, paint)
+            if (BR) canvas.drawRect(w / 2, h / 2, w, h, paint)
+
+            paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+            canvas.drawBitmap(input, 0f, 0f, paint)
+
+            return output
+        }
+    }
+}
---
+++ app/src/main/java/eu/insertcode/portfolio/widgets/ProjectImageView.kt
@@ -0,0 +1,31 @@
+package eu.insertcode.portfolio.widgets
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.util.AttributeSet
+import android.widget.ImageView
+import eu.insertcode.portfolio.utils.BitmapUtil
+
+/**
+ * Created by maartendegoede on 19/10/17.
+ * Copyright © 2017 insertCode.eu. All rights reserved.
+ */
+class ProjectImageView : ImageView {
+
+    constructor(ctx: Context) : this(ctx, null)
+
+    constructor(ctx: Context, attrs: AttributeSet?) : this(ctx, attrs, 0)
+    constructor(ctx: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(ctx, attrs, defStyleAttr)
+
+    override fun draw(canvas: Canvas) {
+        val drawable = drawable as BitmapDrawable? ?: return
+
+        val b = drawable.bitmap
+        val bitmap = b.copy(Bitmap.Config.ARGB_8888, true)
+
+        val roundBitmap = BitmapUtil.getRoundedCornerBitmap(context, bitmap, 16, width, height, false, true, false, true)
+        canvas.drawBitmap(roundBitmap, 0f, 0f, null)
+    }
+}
