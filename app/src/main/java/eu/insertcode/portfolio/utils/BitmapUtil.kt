f3c6bcda2eef3248d618f38b4d3f40991db8f4a6
MrTheGood <maarten@degoedeweb.nl>
2017-12-03 20:33:11 +0100

    Update drawer header image

--- app/src/main/java/eu/insertcode/portfolio/utils/BitmapUtil.kt
+++ app/src/main/java/eu/insertcode/portfolio/utils/BitmapUtil.kt
@@ -2,15 +2,23 @@ package eu.insertcode.portfolio.utils
 
 import android.content.Context
 import android.graphics.*
+import android.renderscript.Allocation
+import android.renderscript.Element
+import android.renderscript.RenderScript
+import android.renderscript.ScriptIntrinsicBlur
+import android.support.annotation.ColorRes
+import android.support.v4.content.ContextCompat
 
 /**
  * Created by maartendegoede on 19/10/17.
  * Copyright © 2017 insertCode.eu. All rights reserved.
  */
+@Suppress("unused", "MemberVisibilityCanPrivate")
 class BitmapUtil {
     companion object {
+        private val DEFAULT_BLUR_BITMAP_SCALE = 0.4f
+        private val DEFAULT_BLUR_RADIUS = 7.5f
 
-        @Suppress("unused")
         fun getRoundedCornerBitmap(ctx: Context, input: Bitmap, pixels: Int, width: Int, height: Int) = getRoundedCornerBitmap(ctx, input, pixels, width, height, false, false, false, false)
 
         //TODO: Remove rounded corners on big images
@@ -42,6 +50,36 @@ class BitmapUtil {
             canvas.drawBitmap(input, 0f, 0f, paint)
 
             return output
+        }
+
+        fun getBlurredBitmap(ctx: Context, img: Bitmap) = getBlurredBitmap(ctx, img, DEFAULT_BLUR_BITMAP_SCALE, DEFAULT_BLUR_RADIUS)
+        fun getBlurredBitmap(ctx: Context, img: Bitmap, bitmapScale: Float, blurRadius: Float): Bitmap {
+            val width = Math.round(img.width * bitmapScale)
+            val height = Math.round(img.height * bitmapScale)
+
+            val inputBitmap = Bitmap.createScaledBitmap(img, width, height, false)
+            val outputBitmap = Bitmap.createBitmap(inputBitmap)
+
+            val renderScript = RenderScript.create(ctx)
+            val intrinsicBlur = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript))
+            val tmpIn = Allocation.createFromBitmap(renderScript, inputBitmap)
+            val tmpOut = Allocation.createFromBitmap(renderScript, outputBitmap)
+            intrinsicBlur.setRadius(blurRadius)
+            intrinsicBlur.setInput(tmpIn)
+            intrinsicBlur.forEach(tmpOut)
+            tmpOut.copyTo(outputBitmap)
+
+            return outputBitmap
+        }
+
+        fun getTintedBitmap(ctx: Context, bitmap: Bitmap, @ColorRes tintColor: Int): Bitmap {
+            val drawableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+            val canvas = Canvas(drawableBitmap)
+            val paint = Paint()
+            paint.colorFilter = PorterDuffColorFilter(ContextCompat.getColor(ctx, tintColor), PorterDuff.Mode.SRC_IN)
+            canvas.drawBitmap(drawableBitmap, 0f, 0f, paint)
+
+            return drawableBitmap
         }
     }
 }
