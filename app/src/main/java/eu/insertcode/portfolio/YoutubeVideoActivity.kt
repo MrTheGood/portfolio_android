175d84bc90beece35b6965cb178929e9707b1a81
MrTheGood <maarten@degoedeweb.nl>
2018-01-12 21:24:15 +0100

    Finished YoutubeVideoActivity

--- app/src/main/java/eu/insertcode/portfolio/YoutubeVideoActivity.kt
+++ app/src/main/java/eu/insertcode/portfolio/YoutubeVideoActivity.kt
@@ -2,6 +2,9 @@ package eu.insertcode.portfolio
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Toast
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
@@ -16,23 +19,40 @@ class YoutubeVideoActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedL
         val RECOVERY_REQUEST = 1
     }
 
+    private lateinit var video: String
+    private var player: YouTubePlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setWindowFlags()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_youtube_video)
+
+        video = intent.extras.getString(EXTRA_VIDEO)
 
         youtube_view.initialize(API_KEY, this)
     }
 
+    private fun setWindowFlags() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                )
+    }
 
     override fun onInitializationSuccess(provider: YouTubePlayer.Provider?, player: YouTubePlayer, wasRestored: Boolean) {
         if (!wasRestored) {
-            player.cueVideo("USQ0RdSqwmU")
-            //TODO: get youtube url from data
-            //TODO: https://www.sitepoint.com/using-the-youtube-api-to-embed-video-in-an-android-app/
+            player.loadVideo(video)
+            this.player = player
         }
     }
 
-    override fun onInitializationFailure(provider: YouTubePlayer.Provider, errorReason: YouTubeInitializationResult) {
+    override fun onInitializationFailure(provider: YouTubePlayer.Provider?, errorReason: YouTubeInitializationResult) {
         if (errorReason.isUserRecoverableError) {
             errorReason.getErrorDialog(this, RECOVERY_REQUEST).show()
         } else {
