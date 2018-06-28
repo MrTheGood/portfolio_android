457caae9579e9ddfd38dde699e34873853f2f7cc
MrTheGood <maarten@degoedeweb.nl>
2018-06-28 18:02:52 +0200

    Reformat some classes

--- app/src/main/java/eu/insertcode/portfolio/view/activity/YoutubeVideoActivity.kt
+++ app/src/main/java/eu/insertcode/portfolio/view/activity/YoutubeVideoActivity.kt
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2018 Maarten de Goede
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package eu.insertcode.portfolio.view.activity
 
 import android.content.Intent
@@ -18,10 +34,10 @@ import kotlinx.android.synthetic.main.activity_youtube_video.*
  */
 class YoutubeVideoActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
     companion object {
-        val EXTRA_VIDEO = "EXTRA_VIDEO"
-        val API_KEY = "AIzaSyD-JqVSPfQfWtPWnADNDm5e4LrJw8adQEI"
+        const val EXTRA_VIDEO = "EXTRA_VIDEO"
+        const val API_KEY = "AIzaSyD-JqVSPfQfWtPWnADNDm5e4LrJw8adQEI"
 
-        val RECOVERY_REQUEST = 1
+        const val RECOVERY_REQUEST = 1
     }
 
     private lateinit var video: String
--- app/src/main/java/eu/insertcode/portfolio/view/view/Project.kt
+++ app/src/main/java/eu/insertcode/portfolio/view/view/Project.kt
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2018 Maarten de Goede
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package eu.insertcode.portfolio.view.view
 
 import android.annotation.SuppressLint
@@ -87,7 +103,7 @@ class Project : FrameLayout {
             TagUtils.addProjectTag(project.tags[it], it, context, projectTags)
         }
 
-        expandButton.setOnClickListener({
+        expandButton.setOnClickListener {
             TransitionManager.beginDelayedTransition(this.parent as ViewGroup)
             if (expanded) {
                 projectFullDescription.visibility = View.GONE
@@ -98,7 +114,7 @@ class Project : FrameLayout {
                 expanded = !expanded
                 expandButton.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_collapse))
             }
-        })
+        }
     }
 
     private fun addProjectImage(adapter: ProjectImagesPagerAdapter, img: MediaItem) {
