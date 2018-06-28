4de9590de056b46f9b2b2fb1c49c3f20eb3aefba
MrTheGood <maarten@degoedeweb.nl>
2018-06-28 19:36:19 +0200

    Reformat Project and Utils

--- app/src/main/java/eu/insertcode/portfolio/view/fragment/AboutFragment.kt
+++ app/src/main/java/eu/insertcode/portfolio/view/fragment/AboutFragment.kt
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2018 Maarten de Goede
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package eu.insertcode.portfolio.view.fragment
 
 import android.content.Context
@@ -18,7 +34,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import eu.insertcode.portfolio.R
-import eu.insertcode.portfolio.utils.Utils
+import eu.insertcode.portfolio.utils.spanHtml
 import kotlinx.android.synthetic.main.fragment_about.view.*
 
 /**
@@ -51,7 +67,7 @@ class AboutFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         layout = inflater.inflate(R.layout.fragment_about, container, false) as ViewGroup
 
-        layout.about_text.text = Utils.fromHtmlCompat(aboutMeText)
+        layout.about_text.text = aboutMeText.spanHtml()
         layout.about_mail_btn.setOnClickListener {
             val options = ActivityOptionsCompat.makeClipRevealAnimation(
                     layout,
