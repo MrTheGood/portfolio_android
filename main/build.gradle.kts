bff25228b6a16e4eb25bddb4f5a9a2d579a7d912
MrTheGood <maarten@degoedeweb.nl>
2019-09-21 12:27:07 +0200

    Initialize multiplatform

---
+++ main/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.library")
+    kotlin("multiplatform")
+}
+
+android {
+    compileSdkVersion(28)
+
+    defaultConfig {
+        minSdkVersion(16)
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+
+        getByName("debug") {
+            isDebuggable = false
+            isMinifyEnabled = false
+
+            matchingFallbacks = listOf("release")
+        }
+    }
+
+    sourceSets["main"].apply {
+        manifest.srcFile("src/androidMain/AndroidManifest.xml")
+        java.srcDirs("src/androidMain/kotlin")
+        res.srcDirs("src/androidMain/res")
+    }
+}
+
+kotlin {
+    //todo: add IOS
+
+    android("android")
+
+    sourceSets["commonMain"].dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
+    }
+
+    sourceSets["androidMain"].dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    }
+}
