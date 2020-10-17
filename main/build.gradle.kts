e5c3f6d596d3264d83ec6794cdd0c59231df2236
MrTheGood <maarten@degoedeweb.nl>
2020-10-17 14:29:37 +0200

    Update dependencies

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -19,7 +19,7 @@ android {
             isDebuggable = false
             isMinifyEnabled = false
 
-            matchingFallbacks = mutableListOf("release")
+            matchingFallbacks.add("release")
         }
     }
 
@@ -48,6 +48,6 @@ kotlin {
     sourceSets["androidMain"].dependencies {
         implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
         implementation("org.jetbrains.kotlin:kotlin-stdlib")
-        implementation("com.google.firebase:firebase-firestore:21.5.0")
+        implementation("com.google.firebase:firebase-firestore:21.7.1")
     }
 }
